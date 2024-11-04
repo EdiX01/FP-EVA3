681ffe70e17edf9f7881c18246c8faa2203b1da3
EdiX01 <L24550375@chihuahua2.tecnm.mx>
2024-11-04 17:45:49 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+/**
+ *
+ * @author EdiX
+ */
+public class EVA3_1_METODOS {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        imprimirMensaje("Hola mundo");
+    }
+    
+    public static void imprimirMensaje(String Msj){
+        System.out.println("Su mensaje es: "+ Msj);
+    }
+    
+}
